15ef599fd474d370a3c4cbb41c485b7e9a86b7f0
sleepless-404 <ousmansohail786@gmail.com>
2025-04-10 14:10:34 +0500

    Variables and Datatypes

---
+++ task1.dart
@@ -0,0 +1,23 @@
+void main() {
+  String name = "Muhammad Ousman";
+  String fname = "Sohail Ahmed";
+  var age = 19;
+  double height = 5.8;
+  bool isEarning = false;
+  String address = " 1st street ,15A/5 ,Bufferzone, Karachi";
+  String phone = "0314-2171538";
+  bool likesDart = true;
+  String studies = "BSCS";
+
+  print("=============PERSONAL INFO==============");
+  print("Name: $name");
+  print("Father Name: $fname");
+  print("Age: $age");
+  print("Height: $height");
+  print("Working a Job: $isEarning");
+  print("Lives at: $address");
+  print("Contact: $phone");
+  print("Likes Dart course: $likesDart");
+  print("Studies: $studies");
+  print("===================================");
+}
